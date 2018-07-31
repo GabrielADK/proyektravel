3d93a4590ba2c48ff414f1181e8ffcf57deedf4f
Gabriele Andhika Dwi K <38529608+GabrielADK@users.noreply.github.com>
2018-08-01 02:50:29 +0700

    perubahan

--- tiket_travel (1).sql
+++ tiket_travel (1).sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `id6650048_tiket_travel`
+-- Database: `tiket_travel`
 --
 
 -- --------------------------------------------------------
